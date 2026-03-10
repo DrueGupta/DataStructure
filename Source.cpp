9db75e06362f3873cbf025ee36903cae4efcdf7e
Drue Gupta <1031762@my.scccd.edu>
2026-03-10 14:43:01 -0700

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -4,7 +4,7 @@
 #include "OrderedList.h"
 using namespace std;
 
-int main() 
+int main()
 {
     List<string> crew;
     crew.insert("Picard");
@@ -18,11 +18,26 @@ int main()
     crew.insert("W. Crusher");
     crew.insert("O'Brien");
     crew.insert("Bob", 5);
-    cout << crew << endl;;
+    cout << crew << endl;
 
     crew.remove();
     crew.remove();
     crew.remove(3);
     cout << crew << endl;
     cout << crew.read(6) << endl;
+
+    OrderedList<string> crew2;
+    crew2.insert("Bob Marley");
+    crew2.insert("Snoop Dog");
+    crew2.insert("Dorritos");
+    crew2.insert("MLG");
+    crew2.insert("AWP", 2);
+    crew2.insert("Call of Duty", 6);
+    cout << crew2 << endl;
+
+    crew2.remove();
+    crew2.remove(3);
+    crew2.remove(6);
+    cout << crew2 << endl;
+    cout << crew2.read(2) << endl;
 }
