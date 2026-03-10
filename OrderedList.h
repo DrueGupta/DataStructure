26c368f6dcb3521a7277b7ab550cbb4ea54ad4c1
Drue Gupta <1031762@my.scccd.edu>
2026-03-10 14:43:17 -0700

    Update OrderedList.h

--- OrderedList.h
+++ OrderedList.h
@@ -4,27 +4,43 @@
 using namespace std;
 
 template <typename TYPE>
-class OrderedList : public List<TYPE>
+class OrderedList : public List<TYPE> 
 {
 public:
-	void insert(TYPE value) override
+	void insert(TYPE value) override 
 	{
-		if(this->size == this->capacity)
-			this->resize():
+		if (this->size == this->capacity) 
+		{
+			this->resize();
+		}
+		int index = 0;
+		for (index = 0; this->data[index] < value && index < this->size; index++) {}
+		for (int i = this->size; i > index; i--) 
+		{
+			this->data[i] = this->data[i - 1];
+		}
+		this->data[index] = value;
+		this->size++;
 	}
-	int index = 0;
-	for (int i = 0; this->data[i] < value && i < this->sie; i++)
-	{
-		index = i;
-	}
-	for (int i = this->size; i > index; i--)
-	{
-		this->date[i] = this->date[i - 1];
-	}
-	this->date[index] = value;
-
-	void insert(TYPE value, int index)
+	void insert(TYPE value, int index) 
 	{
 		this->insert(value);
+	}
+	int search(TYPE value) override
+	{
+		int left = 0;
+		int right = this->size - 1;
+		int middle = (left + right) / 2;
+		while (left < right)
+		{
+			if (this->data[middle] == value)
+				return middle;
+			else if (value < this->data[middle])
+				right = middle - 1;
+			else if (value > this->data[middle])
+				left = middle + 1;
+			middle = (left + right) / 2;
+		}
+		return -1;
 	}
 };
