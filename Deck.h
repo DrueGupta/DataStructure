794d3ec215d4fbf1d246238a516e84cc2bbebdfb
Drue Gupta <1031762@my.scccd.edu>
2026-04-14 15:55:59 -0700

    Update Deck.h

--- Deck.h
+++ Deck.h
@@ -1,1 +1,33 @@
+#pragma once
+#include "List.h"
+#include <iostream>
+using namespace std;
 
+template <typename TYPE>
+class Deck : public List<TYPE>
+{
+	void addFront(TYPE value)
+	{
+		this->insert(value, 0);
+	}
+	void addBack(TYPE value)
+	{
+		this->insert(value);
+	}
+	void removeFront()
+	{
+		this->remove(0);
+	}
+	void removeBack()
+	{
+		this->remove();
+	}
+	TYPE readFront()
+	{
+		return this->read(0);
+	}
+	TYPE readBack()
+	{
+		return this->read(this->size - 1);
+	}
+};
